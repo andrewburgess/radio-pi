9d1b7526a3277936755f4f95ab673c432a02da10
Andrew Burgess <andrew@andrewburgess.io>
2019-07-22 11:24:00 -0700

    :sparkles: feat: open separate player window in electron

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 require("dotenv").config()
 
 import { app, BrowserWindow } from "electron"
+import * as isDev from "electron-is-dev"
 import { Server } from "http"
 
 import startServer from "./lib/server"
@@ -9,6 +10,7 @@ try {
     require("electron-reloader")(module)
 } catch (err) {}
 
+let player: BrowserWindow | null
 let window: BrowserWindow | null
 let server: Server
 
@@ -24,7 +26,67 @@ function createWindow() {
 
     window.loadURL("http://localhost:3000")
     window.on("closed", () => {
+        if (player) {
+            player.close()
+        }
         window = null
+    })
+}
+
+function createPlayerWindow() {
+    let options: Electron.BrowserWindowConstructorOptions = {
+        focusable: false,
+        frame: false,
+        height: 1,
+        opacity: 0,
+        skipTaskbar: true,
+        transparent: true,
+        webPreferences: {
+            backgroundThrottling: false,
+            nodeIntegration: true
+        },
+        width: 1
+    }
+
+    if (isDev) {
+        options = Object.assign({}, options, {
+            focusable: true,
+            frame: true,
+            height: 400,
+            opacity: 1,
+            skipTaskbar: false,
+            transparent: false,
+            width: 400
+        })
+    }
+
+    player = new BrowserWindow(options)
+
+    player.loadURL("http://localhost:3000/player.html")
+    player.on("closed", () => {
+        window = null
+    })
+    player.on("show", () => {
+        if (player) {
+            player.minimize()
+        }
+    })
+    player.webContents.on("new-window", (event, url, frameName, disposition, options, additionalFeatures) => {
+        event.preventDefault()
+        Object.assign(options, {
+            focusable: true,
+            frame: true,
+            height: 600,
+            modal: true,
+            opacity: 1,
+            parent: player,
+            show: true,
+            skipTaskbar: false,
+            transparent: false,
+            width: 800
+        })
+        const newWindow = new BrowserWindow(options)
+        newWindow.loadURL(url)
     })
 }
 
@@ -32,10 +94,15 @@ app.on("activate", () => {
     if (window === null) {
         createWindow()
     }
+
+    if (player === null) {
+        createPlayerWindow()
+    }
 })
 
 app.on("ready", () => {
     createWindow()
+    createPlayerWindow()
 
     server = startServer()
 })
