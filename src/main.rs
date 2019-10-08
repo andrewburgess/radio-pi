ca6cfdeccd2bdb50a63b55111fe148b8cf9c492d
Andrew Burgess <andrew@andrewburgess.io>
2019-10-07 22:42:28 -0700

    :sparkles: feat: begin setup of rust based server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+use actix_web::{middleware, web, App, HttpServer};
+use dotenv::dotenv;
+
+mod spotify;
+
 fn main() {
-    println!("Hello, world!");
+    dotenv().ok();
+
+    HttpServer::new(|| {
+        App::new()
+            .wrap(middleware::DefaultHeaders::new().header("X-Version", "0.1.0"))
+            .wrap(middleware::Compress::default())
+            .wrap(middleware::Logger::default())
+            .service(
+                web::scope("/api/spotify")
+                    .route("/token", web::post().to_async(spotify::token_route))
+                    .route("/url", web::get().to(spotify::get_authorize_url)),
+            )
+    })
+    .bind("127.0.0.1:8080")
+    .unwrap()
+    .run()
+    .unwrap();
 }
