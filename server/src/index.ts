d1ebe2c289525afbbe8155d23d481e325cfd4e98
Andrew Burgess <andrew@andrewburgess.io>
2019-10-11 12:01:13 -0700

    :bug: fix: make sure env vars are defined

--- server/src/index.ts
+++ server/src/index.ts
@@ -4,4 +4,12 @@ import start from "./server"
 
 dotenv.config()
 
+if (!process.env.SPOTIFY_CLIENT_ID) {
+    throw new Error("SPOTIFY_CLIENT_ID not defined")
+}
+
+if (!process.env.SPOTIFY_CLIENT_SECRET) {
+    throw new Error("SPOTIFY_CLIENT_SECRET not defined")
+}
+
 start()
